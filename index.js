8f9e4fa5db349651d973bbd97273e2356130b70e
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-09 02:04:05 +0600

    services data added

--- index.js
+++ index.js
@@ -7,9 +7,34 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// Middle wares : 
 app.use(cors());
 app.use(express.json());
+
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.32rvdcq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// crud oparation : 
+async function run() {
+    try {
+        const serviceCollection = client.db('printigo').collection('services');
+
+        // get all services
+        app.get('/services', async (req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Printigo Web Server')
